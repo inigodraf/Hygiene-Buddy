ab051d7af371482ff67159a3396a723bfb566b9a
inigodraf <119504798+inigodraf@users.noreply.github.com>
2025-11-04 04:02:18 +0800

    VOICE CLONING G!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,6 @@ dependencies {
     implementation("androidx.camera:camera-camera2:1.3.4")
     implementation("androidx.camera:camera-lifecycle:1.3.4")
     implementation("androidx.camera:camera-view:1.3.4")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
